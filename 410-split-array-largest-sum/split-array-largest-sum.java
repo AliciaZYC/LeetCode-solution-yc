a24aa44637990079a636b35cd00ee303cd6ba112
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2024-04-09 17:40:27 -0700

    Time: 1 ms (63.46%) | Memory: 40.9 MB (77.63%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,37 +1,34 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
-        int n = nums.length;
-        int nums_sum=0, max=Integer.MIN_VALUE;
-        for (int num:nums){
-            nums_sum+=num;
-            max=Math.max(num,max);
+        int high = 0;
+        int low = 0;
+        for (int num : nums) {
+            high += num;
+            if (num > low) low = num;
         }
-        int f=max,l=nums_sum, res=Integer.MAX_VALUE;
-        while(f<=l){
-            int m=(f+(l-f)/2);
-            int sum=0,count=1;
-            for(int i=0;i<n;i++)
-            {
-                if(nums[i]+sum<=m)
-                {
-                    sum+=nums[i];
-                }
-                else
-                {
-                    sum=nums[i];
-                    count++;
-                }
-            }
-            if(count<=k)
-            {
-                res=Math.min(res,m);
-                l=m-1;
-            }
-            else
-            {
-                f=m+1;
+
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+            if (check(nums, mid) <= k) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
-        return res;
+        return low;
+    }
+
+    int check(int[] nums, int maxAllowed) {
+        int cut = 1;
+        int cutSum = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (cutSum + nums[i] <= maxAllowed) {
+                cutSum += nums[i];
+            } else {
+                ++cut;
+                cutSum = nums[i];
+            }
+        }
+        return cut;
     }
 }
