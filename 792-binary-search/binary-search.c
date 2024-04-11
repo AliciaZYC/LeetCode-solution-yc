9ac3f1aa97614883efae18b18b413d39dc94c41b
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2024-04-10 19:26:14 -0700

    Time: 25 ms (75.35%) | Memory: 6.9 MB (44.88%) - LeetSync

--- 792-binary-search/binary-search.c
+++ 792-binary-search/binary-search.c
@@ -1,14 +1,10 @@
 int search(int* nums, int numsSize, int target) {
-    int start=0,end=numsSize-1,mid;
+    int start=0, end=numsSize-1,mid;
     while(start<=end){
         mid=(start+end)/2;
-        if(nums[mid]==target) return mid;
-        else if(nums[mid]<target){
-            start=mid+1;
-        }
-        else{
-            end=mid-1;
-        }
+        if (nums[mid]==target) return mid;
+        else if(nums[mid]>target) end=mid-1;
+        else start=mid+1;
     }
     return -1;
 }
