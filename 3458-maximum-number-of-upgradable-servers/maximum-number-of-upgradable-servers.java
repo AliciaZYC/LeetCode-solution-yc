569dcd9258ade439831d591b876ee02070a430f8
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2024-05-25 00:48:35 -0700

    Time: 2 ms (100.00%) | Memory: 66.8 MB (16.28%) - LeetSync

--- 3458-maximum-number-of-upgradable-servers/maximum-number-of-upgradable-servers.java
+++ 3458-maximum-number-of-upgradable-servers/maximum-number-of-upgradable-servers.java
@@ -1,25 +1,10 @@
 class Solution {
     public int[] maxUpgrades(int[] count, int[] upgrade, int[] sell, int[] money) {
-        int n=count.length;
-        int[] ans=new int[n];
-        for(int i=0;i<n;++i){
-            ans[i]=getMax(count[i],upgrade[i],sell[i],money[i]);
+        int n = count.length;
+        int[] result = new int[n];
+        for(int i = 0;i<n;i++){
+            result[i] = Math.min(count[i], (int)((money[i]+(long)sell[i]*count[i])/((long)upgrade[i]+sell[i])));
         }
-        return ans;
-    }
-    private int getMax(long servers,long eachUpgradeCost,long eachSellCost, long money){
-        long low=0,high=servers,ans=0;
-        while(low<=high){
-            long mid=low+(high-low)/2;
-            long totalCost=mid*eachUpgradeCost;
-            long totalMoney=money+(servers-mid)*eachSellCost;
-            if (totalMoney>=totalCost){
-                ans=mid;
-                low=mid+1;
-            }else{
-                high=mid-1;
-            }
-        }
-        return (int) ans;
+        return result;
     }
 }
