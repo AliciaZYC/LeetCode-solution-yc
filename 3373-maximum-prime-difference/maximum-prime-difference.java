54dc4c62456de4237dae8a4f1fb9ebc7a59f861b
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2024-05-25 22:46:11 -0700

    Time: 1 ms (99.91%) | Memory: 79.6 MB (26.44%) - LeetSync

--- 3373-maximum-prime-difference/maximum-prime-difference.java
+++ 3373-maximum-prime-difference/maximum-prime-difference.java
@@ -1,24 +1,24 @@
 class Solution {
-    public int maximumPrimeDifference(int[] nums) {
-        int minIndex=-1,maxIndex=-1;
-        for(int i=0;i<nums.length;i++){
-            if(isPrime(nums[i])){
-                if(minIndex==-1){
-                    minIndex=i;
+    static boolean isNotPrime[] = new boolean[101];
+    static {
+        isNotPrime[1] = true;
+        for (int i = 2; i <= Math.sqrt(isNotPrime.length - 1); i++) {
+            if (!isNotPrime[i]) {
+                for (int j = i * i; j < isNotPrime.length; j += i) {
+                    isNotPrime[j] = true;
                 }
-                maxIndex=i;
             }
         }
-        if(minIndex!=-1)return maxIndex-minIndex;
-        return 0;
     }
-    boolean isPrime(int number){
-        if (number<=1) return false;
-        for(int i=2;i*i<=number;i++){
-            if(number%i==0){
-                return false;
-            }
+
+    public int maximumPrimeDifference(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (isNotPrime[nums[left]]) {
+            left++;
         }
-        return true;
+        while (isNotPrime[nums[right]]) {
+            right--;
+        }
+        return right - left;
     }
 }
