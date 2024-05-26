182a7f08cdfa3e5ecfab7abf8412ed386c6770b4
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2024-05-25 20:26:12 -0700

    Time: 16 ms (38.30%) | Memory: 79.5 MB (31.52%) - LeetSync

---
+++ 3373-maximum-prime-difference/maximum-prime-difference.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maximumPrimeDifference(int[] nums) {
+        int minIndex=-1,maxIndex=-1;
+        for(int i=0;i<nums.length;i++){
+            if(isPrime(nums[i])){
+                if(minIndex==-1){
+                    minIndex=i;
+                }
+                maxIndex=i;
+            }
+        }
+        if(minIndex!=-1)return maxIndex-minIndex;
+        return 0;
+    }
+    boolean isPrime(int number){
+        if (number<=1) return false;
+        for(int i=2;i*i<=number;i++){
+            if(number%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
