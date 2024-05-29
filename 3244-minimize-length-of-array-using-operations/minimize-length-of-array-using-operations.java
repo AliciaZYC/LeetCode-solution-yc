8f6f182976863b723f1ad98a239337c8e88f7505
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2024-05-28 22:53:52 -0700

    Time: 2 ms (95.15%) | Memory: 58.4 MB (59.22%) - LeetSync

--- 3244-minimize-length-of-array-using-operations/minimize-length-of-array-using-operations.java
+++ 3244-minimize-length-of-array-using-operations/minimize-length-of-array-using-operations.java
@@ -1,17 +1,17 @@
 class Solution {
     public int minimumArrayLength(int[] nums) {
-        Arrays.sort(nums);
-        int n=nums.length;
-        int c=1,g=Integer.MAX_VALUE;
-        for (int i=1;i<n;i++){
-            g=Math.min(g,gcd(nums[i],nums[i-1]));
-            if(g<nums[0])return 1;
-            if(nums[i]==nums[0])c++;
+        int min = Integer.MAX_VALUE;
+        for(int i : nums){
+            min = Math.min(min,i);
         }
-        int k=(int)Math.ceil(c/(2.0));
-        return k;
-    }
-    private int gcd(int a, int b){
-        return b==0?a:gcd(b,a%b);
+        int cnt_min = 0;
+        for(int i : nums){
+            if(i%min>0)return 1;
+            if(min == i){
+                cnt_min++;
+            }
+        }
+        if(cnt_min==1)return 1;
+        return (cnt_min+1)/2; 
     }
 }
