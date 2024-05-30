4601f6dcdf213d7cdd61f849e03a9168a04ebf73
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2024-05-29 19:03:03 -0700

    Time: 12 ms (96.18%) | Memory: 44.6 MB (16.39%) - LeetSync

--- 3210-count-beautiful-substrings-i/count-beautiful-substrings-i.java
+++ 3210-count-beautiful-substrings-i/count-beautiful-substrings-i.java
@@ -1,18 +1,33 @@
 class Solution {
     public int beautifulSubstrings(String s, int k) {
-        int count = 0, n = s.length();
-        for (int i = 0; i < n; i++) {
-            int vowels = 0, conso = 0;
-            for (int j = i; j < n; j++) {
-                if (isVowel(s.charAt(j))) vowels++;
-                else conso++;
-                if (vowels == conso && (vowels * conso) % k == 0) count++;
-            }
-        }
-        return count;
-    }
+        HashMap<Integer , List<Integer>> map = new HashMap<>();
+        map.put(0 , new ArrayList());
+        map.get(0).add(-1); //empty substring
 
-    public boolean isVowel(char c) {
-        return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+        int con=0;
+        int vow=0;
+        int res=0;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i'||ch=='o'| ch=='u'){
+                vow++;
+            }
+            else con++;
+
+            if(map.containsKey(con-vow)){
+                List<Integer> temp = map.get(con-vow);
+                for(int id:temp){
+                    int number = (i-id+1)/2;
+                    if((number*number)%k==0) res++;
+                }
+             map.get(con-vow).add(i);
+            }
+        
+           else{ 
+              map.put(con-vow , new ArrayList<>());
+              map.get(con-vow).add(i);
+           }
+        }
+        return res;
     }
 }
