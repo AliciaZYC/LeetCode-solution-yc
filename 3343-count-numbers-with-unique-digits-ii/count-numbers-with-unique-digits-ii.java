1ee77f85b41c224aafaa78fef6ac1ad6d07c6cad
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2024-06-08 20:21:22 -0700

    Time: 2 ms (98.93%) | Memory: 41.2 MB (89.30%) - LeetSync

--- 3343-count-numbers-with-unique-digits-ii/count-numbers-with-unique-digits-ii.java
+++ 3343-count-numbers-with-unique-digits-ii/count-numbers-with-unique-digits-ii.java
@@ -1,44 +1,28 @@
 class Solution {
     public int numberCount(int a, int b) {
-        int[] nums=get(b);
-        int n=nums.length;
-        int[][][]memo=new int[n+1][1<<10][2];
-        assignDefault(memo);
-        int max = dfs(nums, 0, 0, true, 0, memo);
-        nums = get(a-1); 
-        n = nums.length;
-        memo = new int[n+1][1<<10][2];
-        assignDefault(memo);
-        int min = dfs(nums, 0, 0, true, 0, memo);
-        return max-min;
-    }   
-    int[] get(int x) {
-        int n = String.valueOf(x).length(), i = n-1;
-        int[] nums = new int[n];
-        while(x>0){
-            nums[i--]=x%10;
-            x/=10;
-        }
-        return nums;
-    }
-    void assignDefault(int[][][] memo){
-        for(int[][] memo1:memo){
-            for(int[] memo2:memo1){
-                Arrays.fill(memo2,-1);
+        int count = 0;
+
+
+        for(int i = a; i <= b; i++) {
+            int ones = i % 10;
+            int tens = (i/10) % 10;
+
+
+            int hundreds = i/100;
+
+            if(ones == tens) {
+                continue;
             }
+
+            if(tens == hundreds && tens != 0) {
+                continue;
+            }
+
+            if(hundreds == ones && hundreds != 0) {
+                continue;
+            }
+            count++;
         }
-    }
-    int dfs(int[] nums,int i, int mask,boolean limit,int value, int[][][]memo){
-        if(i==nums.length) return 1;
-        int k=limit?1:0;
-        if(memo[i][mask][k]!=-1) return memo[i][mask][k];
-        int n=limit?nums[i]:9;
-        int sum=0;
-        for(int num=0;num<=n;num++){
-            if((mask&1<<num)!=0) continue;
-            int nextMask=value+num==0?0:mask|1<<num;
-            sum+=dfs(nums,i+1,nextMask,limit&&num==n,value+num,memo);
-        }
-        return memo[i][mask][k]=sum;
+        return count;
     }
 }
