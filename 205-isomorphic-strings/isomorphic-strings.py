1486c559acc8674dcc111be2af9bf21c75c2fa7b
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2025-02-26 21:51:35 -0800

    Time: 2 ms (97.64%) | Memory: 18.1 MB (18.10%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,16 +1,20 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        if len(s)!=len(t) or len(set(s))!=len(set(t)):
-            return False
-        else:
-            hash_map={}
-            for i in range(len(s)):
-                # hash_map[s[i]] = t[i]
-                if s[i] in hash_map.keys():
-                    if t[i] not in hash_map.values() or hash_map[s[i]] != t[i]:
-                        return False
-                elif t[i] in hash_map.values():
-                    return False
-                else:
-                    hash_map[s[i]] = t[i]
-            return True
+        # if len(s)!=len(t) or len(set(s))!=len(set(t)):
+        #     return False
+        # else:
+        #     hash_map={}
+        #     for i in range(len(s)):
+        #         # hash_map[s[i]] = t[i]
+        #         if s[i] in hash_map.keys():
+        #             if t[i] not in hash_map.values() or hash_map[s[i]] != t[i]:
+        #                 return False
+        #         elif t[i] in hash_map.values():
+        #             return False
+        #         else:
+        #             hash_map[s[i]] = t[i]
+        #     return True
+        a = set(zip(s,t))
+        b = set(s)
+        c = set(t)
+        return len(a)==len(b)==len(c)
