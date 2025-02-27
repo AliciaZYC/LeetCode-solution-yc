1912eef7cd0d30fea9d0b39bea6645f1308f6a5d
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2025-02-26 21:48:49 -0800

    Time: 49 ms (6.89%) | Memory: 17.9 MB (80.13%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s)!=len(t) or len(set(s))!=len(set(t)):
+            return False
+        else:
+            hash_map={}
+            for i in range(len(s)):
+                # hash_map[s[i]] = t[i]
+                if s[i] in hash_map.keys():
+                    if t[i] not in hash_map.values() or hash_map[s[i]] != t[i]:
+                        return False
+                elif t[i] in hash_map.values():
+                    return False
+                else:
+                    hash_map[s[i]] = t[i]
+            return True
