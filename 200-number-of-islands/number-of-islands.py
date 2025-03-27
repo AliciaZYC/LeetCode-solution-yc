1f64a760551c716877efac5162fd26bbabd3272e
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2025-03-26 20:30:17 -0700

    Time: 230 ms (91.82%) | Memory: 20.1 MB (82.88%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,6 +2,7 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
+
         def dfs(i,j):
             if i<0 or i>=len(grid) or j<0 or j>=len(grid[0]) or grid[i][j]!="1":
                 return
@@ -10,6 +11,7 @@ class Solution:
             dfs(i+1,j)
             dfs(i,j-1)
             dfs(i,j+1)
+            
         num_of_island=0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
