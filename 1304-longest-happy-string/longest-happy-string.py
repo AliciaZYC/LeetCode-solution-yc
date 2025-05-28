5dbd7f3b0ffa567aaa2208067fda27d5938197ea
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2025-05-27 18:29:16 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (23.50%) - LeetSync

---
+++ 1304-longest-happy-string/longest-happy-string.py
@@ -0,0 +1,34 @@
+class Solution:
+    def longestDiverseString(self, a: int, b: int, c: int) -> str:
+        # scenario 1: cutting length
+        # if a >= 2* (b+c+1) or b >= 2* (a+c+1) or c >= 2* (b+a+1): XX*XX*...*XX
+        curra, currb, currc = 0, 0, 0
+        # Maximum total iterations possible is given by the sum of a, b, and c.
+        total_iterations = a + b + c
+        result = []
+
+        for i in range(total_iterations):
+            # If 'a' is maximum and its streak is less than 2, or if streak of 'b' or 'c' is 2, then 'a' will be the next character.
+            if (a >= b and a >= c and curra != 2) or (a > 0 and (currb == 2 or currc == 2)):
+                result.append("a")
+                a -= 1
+                curra += 1
+                currb = 0
+                currc = 0
+            # If 'b' is maximum and its streak is less than 2, or if streak of 'a' or 'c' is 2, then 'b' will be the next character.
+            elif (b >= a and b >= c and currb != 2) or (b > 0 and (currc == 2 or curra == 2)):
+                result.append("b")
+                b -= 1
+                currb += 1
+                curra = 0
+                currc = 0
+            # If 'c' is maximum and its streak is less than 2, or if streak of 'a' or 'b' is 2, then 'c' will be the next character.
+            elif (c >= a and c >= b and currc != 2) or (c > 0 and (curra == 2 or currb == 2)):
+                result.append("c")
+                c -= 1
+                currc += 1
+                curra = 0
+                currb = 0
+
+        return "".join(result)
+
