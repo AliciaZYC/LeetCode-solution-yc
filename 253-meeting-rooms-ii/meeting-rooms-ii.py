8507473b724b10e44f5c057ab6c0ca6f4137491c
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2025-06-05 15:20:49 -0700

    Time: 4 ms (54.08%) | Memory: 19.9 MB (75.31%) - LeetSync

---
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        if not intervals:
+            return 0
+        n = len(intervals)
+        used_rooms = 0
+        start_timings = sorted([i[0] for i in intervals])
+        end_timings = sorted(i[1] for i in intervals)
+        end_pointer = 0
+        start_pointer = 0
+        while start_pointer < n:
+            if start_timings[start_pointer] >= end_timings[end_pointer]:
+                used_rooms -= 1
+                end_pointer += 1
+            used_rooms += 1    
+            start_pointer += 1   
+        return used_rooms
+        
