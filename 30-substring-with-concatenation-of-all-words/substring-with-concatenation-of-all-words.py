e3852c92d148bc704bc40cfc9f8dc6a1df8603c7
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2025-09-12 18:11:32 -0700

    Time: 23 ms (86.54%) | Memory: 18.8 MB (38.23%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        length = len(words[0])
+        word_count = collections.Counter(words)
+        res = []
+        # i相当于算从哪个char开始算word
+        for i in range(length):
+            start = i
+            window = collections.defaultdict(int)
+            words_used = 0
+            for j in range(i,len(s)-length+1,length):
+                word = s[j:j+length]
+                if word not in word_count:
+                    start = j+length
+                    window = collections.defaultdict(int)
+                    words_used = 0
+                    continue
+                words_used += 1
+                window[word] +=1
+                while window[word]>word_count[word]:
+                    window[s[start:start+length]]-=1
+                    start += length
+                    words_used -=1
+                if words_used == len(words):
+                    res.append(start)
+        return res
