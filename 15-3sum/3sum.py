aae2e4e05bbbe67e153f753066f81ed79558c7cf
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2025-09-12 17:33:38 -0700

    Time: 811 ms (22.39%) | Memory: 21.4 MB (12.41%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -3,35 +3,36 @@ from typing import List
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        return self.nSumTarget(nums, 3, 0, 0)
+        return self.nSumTarget(nums,3,0,0)
     def nSumTarget(self, nums: List[int], n: int, start: int, target: int) -> List[List[int]]:
-        sz = len(nums)
+        l = len(nums)
         res = []
-        if n < 2 or sz < n:
+        if n<2 or n>l:
             return res
+        # base case
         if n == 2:
-            lo, hi = start, sz - 1
-            while lo < hi:
-                sum_val = nums[lo] + nums[hi]
-                left, right = nums[lo], nums[hi]
-                if sum_val < target:
-                    # use while since there may be same value
-                    while lo < hi and nums[lo] == left:
-                        lo += 1
-                elif sum_val > target:
-                    while lo < hi and nums[hi] == right:
-                        hi -= 1
+            low,high=start,l-1
+            while low<high:
+                sum_val = nums[low]+nums[high]
+                left=nums[low]
+                right=nums[high]
+                if sum_val<target:
+                    while low<high and nums[low]==left:
+                        low+=1
+                elif sum_val>target:
+                    while low<high and nums[high]==right:
+                        high -=1
                 else:
-                    res.append([left, right])
-                    while lo < hi and nums[lo] == left:
-                        lo += 1
-                    while lo < hi and nums[hi] == right:
-                        hi -= 1
+                    res.append([left,right])
+                    while low<high and nums[low]==left:
+                        low+=1
+                    while low<high and nums[high]==right:
+                        high -=1
         else:
-            for i in range(start, sz):
-                if i > start and nums[i] == nums[i - 1]:
+            for i in range(start,l):
+                if i>start and nums[i]==nums[i-1]:
                     continue
-                sub = self.nSumTarget(nums, n - 1, i + 1, target - nums[i])
+                sub = self.nSumTarget(nums,n-1,i+1,target-nums[i])
                 for arr in sub:
                     arr.append(nums[i])
                     res.append(arr)
