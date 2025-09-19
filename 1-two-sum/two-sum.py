4be9b6d15a0a07ed4b9d70ce1bc31afb5555c4de
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2025-09-18 19:34:12 -0700

    Time: 0 ms (100.00%) | Memory: 18.8 MB (50.80%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,7 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        res={}
-        for i in range (len(nums)):
-            if target - nums[i] in res:
-                return [i,res[target - nums[i]]]
-            res[nums[i]]=i
+        # 维护 val -> index 的映射
+        val_to_index = {}
+        for i in range(len(nums)):
+            # 查表，看看是否有能和 nums[i] 凑出 target 的元素
+            need = target - nums[i]
+            if need in val_to_index:
+                return [val_to_index[need], i]
+            # 存入 val -> index 的映射
+            val_to_index[nums[i]] = i
+        return []
