84cd58e75768a6001723df2e22269de108da2e6a
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2025-11-02 12:55:42 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (80.38%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -1,14 +1,27 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        heap = []
-        for stone in stones:
-            heap.append(-stone)
-        heapify(heap)
-        while len(heap)>1:
-            s1 = heappop(heap)
-            s2 = heappop(heap)
-            if s1 != s2:
-                heappush(heap,s1-s2)
-        if heap:
-            return -heap[0]
-        return 0
+        # heap = []
+        # for stone in stones:
+        #     heap.append(-stone)
+        # heapify(heap)
+        # while len(heap)>1:
+        #     s1 = heappop(heap)
+        #     s2 = heappop(heap)
+        #     if s1 != s2:
+        #         heappush(heap,s1-s2)
+        # if heap:
+        #     return -heap[0]
+        # return 0
+        # first, negate all weight values in-place
+        for i, s in enumerate(stones):
+            stones[i] = -s
+        heapify(stones)  # pass all negated values into the min-heap
+        while stones:
+            s1 = -heappop(stones)  # the heaviest stone
+            if not stones:  # s1 is the remaining stone
+                return s1
+            s2 = -heappop(stones)  # the second-heaviest stone; s2 <= s1
+            if s1 > s2:
+                heappush(stones, s2-s1)  # push the NEGATED value of s1-s2; i.e., s2-s1
+            # else s1 == s2; both stones are destroyed
+        return 0  # if no more stones remain
