f6b46ef44118c21650ecdaa148491e7b857211b7
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2025-11-18 17:25:42 -0800

    Time: 31 ms (19.88%) | Memory: 19.4 MB (38.89%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -7,12 +7,12 @@ class Solution:
         r_max = [0]*n
         l_max[0] = height[0]
         for i in range(1,n):
-            l_max[i] = max(l_max[i-1],height[i])
+            l_max[i]= max(l_max[i-1],height[i])
         r_max[n-1] = height[n-1]
         for i in range(n-2,-1,-1):
             r_max[i] = max(r_max[i+1],height[i])
         res = 0
         for i in range(n):
             water_level = min(l_max[i],r_max[i])
-            res += water_level-height[i]
+            res += water_level - height[i]
         return res
