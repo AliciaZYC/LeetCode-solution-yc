1cbe496875fe9c1176d8b891e0f3f2c5d1173578
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2026-02-25 17:39:56 -0800

    Time: 7 ms (57.51%) | Memory: 21.1 MB (74.00%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,17 +1,18 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # target = 0
-        # count =0
-        # for i in range(len(nums)):
-        #     if count == 0:
-        #         target = nums[i]
-        #         count =1
-        #     elif target != nums[i]:
-        #         count -=1
-        #     else:
-        #         count +=1
-        # return target
+        target = 0
+        count =0
+        for i in range(len(nums)):
+            if count == 0:
+                target = nums[i]
+                count =1
+            # count ！= 0
+            elif target != nums[i]:
+                count -=1
+            else:
+                count +=1
+        return target
         # 上面这个的逻辑是：majarity的一定是出现数量大于1/2的，就是它可以被任何一个其他的抵消，但是仍有多
-        counts = collections.Counter(nums)
-        return max(counts.keys(),key=counts.get)
+        # counts = collections.Counter(nums)
+        # return max(counts.keys(),key=counts.get)
       
