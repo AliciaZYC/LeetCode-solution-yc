deb946687ea2bfb3f0b3d06ba08164525fcfc811
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2026-02-25 17:35:27 -0800

    Time: 9 ms (35.14%) | Memory: 22.6 MB (61.97%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,10 +1,20 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        sort_re=sorted(intervals, key=lambda x: x[0])
-        for i in range(1,len(intervals)):
-          if sort_re[i][0]<=sort_re[i-1][1]:
-            sort_re[i]=[sort_re[i-1][0],max(sort_re[i-1][1],sort_re[i][1])]
-            sort_re[i-1]=0
-          i+=1
-        re = [item for item in sort_re if item != 0]
-        return re
+        # 按照开始时间排序
+        intervals.sort(key=lambda x: x[0])
+        # 初始化结果列表，放入第一个区间
+        merged = [intervals[0]]
+        # 从第二个区间开始遍历
+        for i in range(1, len(intervals)):
+            # 获取结果列表中最后一个区间
+            last_interval = merged[-1]
+            current_interval = intervals[i]
+            # 如果当前区间的开始时间 <= 最后一个区间的结束时间，则合并
+            if current_interval[0] <= last_interval[1]:
+                # 更新最后一个区间的结束时间为两个区间结束时间的最大值
+                merged[-1][1] = max(last_interval[1], current_interval[1])
+            else:
+                # 如果不重叠，直接添加当前区间
+                merged.append(current_interval)
+        return merged
+
