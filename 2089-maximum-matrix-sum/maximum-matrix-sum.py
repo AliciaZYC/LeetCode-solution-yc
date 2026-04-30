fca146bf1853b720c9ff18a3ed13b4ad249a6542
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2026-04-30 15:34:39 -0700

    Time: 78 ms (28.59%) | Memory: 29.1 MB (8.98%) - LeetSync

---
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxMatrixSum(self, matrix: List[List[int]]) -> int:
+        # 理论是：
+        # 偶数个负数的话，是所有abs的和
+        # 奇数个负数的话，是除了最大负数的所有abs之和 + 这个最大负数
+        n = len(matrix)
+        count = 0
+        track = float("inf")
+        res = 0
+        for x in range(n):
+            for y in range(n):
+                res += abs(matrix[x][y])
+                if matrix[x][y] < 0:
+                    count += 1
+                track = min(track,abs(matrix[x][y]))
+        if count%2 != 0:
+            res -= 2* track
+        return res
