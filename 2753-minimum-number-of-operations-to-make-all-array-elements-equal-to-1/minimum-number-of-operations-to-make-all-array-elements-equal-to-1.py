73eb9c3aa80954ca4f57e3886e06fbe24538ddf7
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2026-05-04 16:55:06 -0700

    Time: 4 ms (16.76%) | Memory: 19.4 MB (45.95%) - LeetSync

---
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        # all share same gcd -> -1
+        # when there is 1 in array, #operations += #num!= 1
+        n = len(nums)
+        num1 = 0
+        g = 0
+        for num in nums:
+            if num == 1:
+                num1 += 1
+            g = gcd(g,num)
+        if num1 > 0:
+            return n - num1
+        if g > 1:
+            return -1
+        min_len = n
+        for i in range(n):
+            g = 0
+            for j in range(i,n):
+                g = gcd(g,nums[j])
+                if g == 1:
+                    min_len = min(min_len, j - i + 1)
+                    break
+        return min_len + n - 2
