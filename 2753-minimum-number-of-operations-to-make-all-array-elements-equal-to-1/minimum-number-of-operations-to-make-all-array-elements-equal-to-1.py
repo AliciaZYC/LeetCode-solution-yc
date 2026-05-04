cf9ff0f324a794fe6d7a3158896dc8137f02fde2
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2026-05-04 16:55:47 -0700

    Time: 4 ms (16.76%) | Memory: 19.3 MB (82.37%) - LeetSync

--- 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.py
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.py
@@ -5,20 +5,24 @@ class Solution:
         n = len(nums)
         num1 = 0
         g = 0
-        for num in nums:
-            if num == 1:
+
+        for x in nums:
+            if x == 1:
                 num1 += 1
-            g = gcd(g,num)
+            g = gcd(g, x)
+
         if num1 > 0:
             return n - num1
         if g > 1:
             return -1
+
         min_len = n
         for i in range(n):
             g = 0
-            for j in range(i,n):
-                g = gcd(g,nums[j])
+            for j in range(i, n):
+                g = gcd(g, nums[j])
                 if g == 1:
                     min_len = min(min_len, j - i + 1)
                     break
+
         return min_len + n - 2
