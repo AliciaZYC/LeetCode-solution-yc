b66271832c698d9a6b1af3014a29cb0d27665fc1
Yichi Zhang <118243283+AliciaZYC@users.noreply.github.com>
2026-05-05 18:22:05 -0700

    Time: 95 ms (79.91%) | Memory: 69 MB (68.04%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -1,21 +1,18 @@
 class TimeMap:
 
     def __init__(self):
-        self.key_time_map = {}
+        self.d = collections.defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if not key in self.key_time_map:
-            self.key_time_map[key] = SortedDict()
-        self.key_time_map[key][timestamp] = value
+        self.d[key].append((timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
-        if not key in self.key_time_map:
-            return ""
-        it = self.key_time_map[key].bisect_right(timestamp)
-        if it == 0:
-            return ""
-        return self.key_time_map[key].peekitem(it -1)[1]
-
+        if key not in self.d: return ''
+        arr = self.d[key]
+        # chr(127) (ASCII DEL) is greater than all printable characters 
+        idx = bisect.bisect_right(arr, (timestamp, chr(127)))
+        return arr[idx-1][1] if idx>0 else ""
+        
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
 # obj.set(key,value,timestamp)
